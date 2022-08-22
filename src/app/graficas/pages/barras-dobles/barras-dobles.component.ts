e8d7fe685320db2ab04f7aa50ed2106a434672f5
lzamranoa <lzambranoa.software@gmail.com>
2022-08-22 16:39:18 -0500

    Creación del proyecto y de su estructura

---
+++ src/app/graficas/pages/barras-dobles/barras-dobles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barras-dobles',
+  templateUrl: './barras-dobles.component.html',
+  styleUrls: ['./barras-dobles.component.css']
+})
+export class BarrasDoblesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
